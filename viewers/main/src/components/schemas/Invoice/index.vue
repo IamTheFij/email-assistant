63f2d2690f849cb4b21eed60473d998dedcc556e
Phyks (Lucas Verney) <phyks@phyks.me>
2018-05-24 14:29:34 +0200

    [Viewer] Do not fail if customer is not available for an Invoice

--- viewers/main/src/components/schemas/Invoice/index.vue
+++ viewers/main/src/components/schemas/Invoice/index.vue
@@ -64,7 +64,7 @@ export default {
                 id: item.metadata.identifier,
                 date: moment(item.metadata.date).local().format('HH:mm DD/MM/YYYY'),
                 name: item.metadata.name,
-                customer: item.metadata.customer.name,
+                customer: item.metadata.customer ? item.metadata.customer.name : null,
                 url: item.metadata.url,
                 filename,
             };
